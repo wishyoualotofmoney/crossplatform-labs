1d3fa1c0525ca22c2cc8c1eb7ed8304035c0a678
Ангелина Балаян <bangel28@mail.ru>
2024-10-20 14:29:02 +0300

    Добавьте файлы проекта.

---
+++ lab1-gazizov/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace lab1_gazizov.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
